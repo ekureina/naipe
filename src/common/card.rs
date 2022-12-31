6c254b2f2a3836b20efb07ae70ba8ac8bea8c9e6
Claire Moore <nrmoore@uci.edu>
2022-12-31 08:41:48 -0800

    feat: Add a suitless wrapper around cards

--- src/common/card.rs
+++ src/common/card.rs
@@ -1,3 +1,8 @@
+//! A module for basic card structs
+//!
+//! Includes Ranks, Suits, and Cards themselves
+//! Also includes wrappers that change collation properties
+
 use std::fmt::{self, Display, Formatter};
 
 /// An enum representing the rank of a card
@@ -134,10 +139,10 @@ impl Display for Suit {
 }
 
 /// A struct representing a card
-#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+#[derive(Clone, Copy, Eq, PartialEq, Debug, PartialOrd, Ord)]
 pub struct Card {
-    rank: Rank,
     suit: Suit,
+    rank: Rank,
 }
 
 impl Card {
@@ -190,5 +195,80 @@ impl Card {
 impl Display for Card {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "{}{}", self.rank, self.suit)
+    }
+}
+
+/// A wrapper around a card that does not compare suits
+/// # Usage
+/// ```
+/// # use naipe::common::card::{Card, Rank, Suit, Suitless};
+/// let card = Suitless(Card::new(Suit::Spade, Rank::Ace));
+/// ```
+#[derive(Clone, Copy, Debug)]
+#[repr(transparent)]
+pub struct Suitless(pub Card);
+
+impl Suitless {
+    /// Gets the rank of the given card
+    /// # Examples
+    /// ```
+    /// # use naipe::common::card::{Card, Rank, Suit, Suitless};
+    /// let card = Suitless(Card::new(Suit::Spade, Rank::Ace));
+    /// assert_eq!(card.get_rank(), Rank::Ace);
+    /// ```
+    pub fn get_rank(&self) -> Rank {
+        self.0.rank
+    }
+
+    /// Gets the suit of the given card
+    /// # Examples
+    /// ```
+    /// # use naipe::common::card::{Card, Rank, Suit, Suitless};
+    /// let card = Suitless(Card::new(Suit::Spade, Rank::Ace));
+    /// assert_eq!(card.get_suit(), Suit::Spade);
+    /// ```
+    pub fn get_suit(&self) -> Suit {
+        self.0.suit
+    }
+
+    /// Unwraps this card and gets the underlying card
+    ///
+    /// Consumes this wrapper
+    /// # Examples
+    /// ```
+    /// # use naipe::common::card::{Card, Rank, Suit, Suitless};
+    /// let card = Suitless(Card::new(Suit::Spade, Rank::Ace));
+    /// let unwrapped = card.unwrap();
+    /// assert_eq!(unwrapped.get_suit(), Suit::Spade);
+    /// assert_eq!(unwrapped.get_rank(), Rank::Ace);
+    /// ```
+    pub fn unwrap(self) -> Card {
+        self.0
+    }
+}
+
+impl PartialOrd for Suitless {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Suitless {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.rank.cmp(&other.0.rank)
+    }
+}
+
+impl PartialEq for Suitless {
+    fn eq(&self, other: &Suitless) -> bool {
+        self.0.rank == other.0.rank
+    }
+}
+
+impl Eq for Suitless {}
+
+impl Display for Suitless {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
